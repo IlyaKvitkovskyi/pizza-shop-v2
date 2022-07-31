b7bbfe5229a74cf65ce5956fe06eb0f3209a0d7d
Illia <ilya.kvitkovskyi@gmail.com>
2022-07-31 19:32:56 +0300

    added pizza list render and pizza size type/size selection

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzas from './assets/pizzas.json';
+
 import './scss/app.scss';
 
 function App() {
@@ -18,8 +20,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock title="Мексиканская" price={310} />
-            <PizzaBlock title="Мексиканская" price={310} />
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} />
+            ))}
           </div>
         </div>
       </div>
