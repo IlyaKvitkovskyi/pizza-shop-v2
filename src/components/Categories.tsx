590a5483bac5908503f07039e1270a6746e104a2
Illia Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2023-04-12 14:09:21 +0300

    added tipization for props in Categories

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 
-export default function Categories({ value, onChangeCategory }) {
+type CategoriesProps = {
+  value: number;
+  onChangeCategory: any;
+};
+
+const Categories: React.FC<CategoriesProps> = ({ value, onChangeCategory }) => {
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
   return (
     <div className="categories">
       <ul>
         {categories.map((categoryName, i) => (
-          <li
-            key={i}
-            onClick={() => onChangeCategory(i)}
-            className={value === i ? 'active' : ''}>
+          <li key={i} onClick={() => onChangeCategory(i)} className={value === i ? 'active' : ''}>
             {categoryName}
           </li>
         ))}
       </ul>
     </div>
   );
-}
+};
+
+export default Categories;
